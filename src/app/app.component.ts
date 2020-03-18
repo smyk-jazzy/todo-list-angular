94c78a487d6bba67190cee5b4f88896e67e9d2ff
Smyk <tomasz.jeziorny@wp.pl>
2020-03-19 00:49:59 +0100

    udemy *ngfor 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // jeszcze tutaj nic nie ma
+  cleanLabelStr = 'Wyczyść wszystkie';
+  config: { [key: string]: string | Date } = null;
+  tasks: Task[] = [
+    {
+      name: 'Spacer z kotem Sylwestrem',
+      deadline: '2020-03-20',
+      done: false,
+    },
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-06',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false,
+    },
+  ];
+  constructor() {
+    // symulacja ładowania danych z opóźnieniem
+    setTimeout(() => this.initConfig(), 2000);
+    console.log(this.tasks);
+  }
+
+  initConfig() {
+    this.config = {
+      title: 'Lista zadań Smyka',
+      footer:
+        '© Lista zadań, All rights reserved (4300: todo-list-udemy-master 2).',
+      date: new Date(),
+    };
+  }
+
+  //getter - typescript
+  get cleanLabel(): string {
+    return this.cleanLabelStr;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
+import { NgPipesModule } from 'ngx-pipes';
+import localePl from '@angular/common/locales/pl';
+import { registerLocaleData} from '@angular/common';
+
+registerLocaleData(localePl);
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgPipesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
