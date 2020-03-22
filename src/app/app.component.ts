f7ce8b66ab12d2c603e796cc4094b02e4dd3b4de
Smyk <tomasz.jeziorny@wp.pl>
2020-03-22 16:28:28 +0100

    ng-model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from './task';
+import { OrderByPipe } from 'ngx-pipes';
+import { ReversePipe } from 'ngx-pipes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   cleanLabelStr = 'Wyczyść wszystkie';
   config: { [key: string]: string | Date } = null;
+  taskName = 'domyślne zadanie';
+  taskDate = '';
   tasks: Task[] = [
     {
       name: 'Spacer z kotem Sylwestrem',
@@ -31,10 +35,17 @@ export class AppComponent {
       done: false,
     },
   ];
+  inputTest = 'pusto';
   constructor() {
     // symulacja ładowania danych z opóźnieniem
     setTimeout(() => this.initConfig(), 2000);
     console.log(this.tasks);
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.sortTasks();
   }
 
   initConfig() {
@@ -50,4 +61,49 @@ export class AppComponent {
   get cleanLabel(): string {
     return this.cleanLabelStr;
   }
+
+  cleanTasks() {
+    this.tasks = [];
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    const target = event.target as HTMLInputElement;
+    this.inputTest = target.value;
+  }
+
+  newTask(taskName: HTMLInputElement, taskDeadline : HTMLInputElement) {
+    this.addTask(taskName.value, taskDeadline.value);
+    taskName.value = '';
+    taskDeadline.value = '';
+  }
+
+ createTask() {
+  this.addTask(this.taskName, this.taskDate);
+  this.taskName = 'dodano 2';
+  this.taskDate = '2019-11-29';
+ }
+
+  addTask(taskName: string, taskDeadline: string) {
+    console.log('taskName: ' + this.taskName);
+    console.log('taskDate: ' + this.taskDate);
+    if (taskDeadline.trim() === ''){
+        taskDeadline = new Date().toString();
+    }
+    this.tasks.push({
+      name: taskName,
+      deadline: taskDeadline,
+      done: false,
+    });
+    this.sortTasks();
+  }
+
+  sortTasks() {
+    this.tasks = this.tasks.sort(this.compareTasks);
+  }
+
+  compareTasks(first: Task, second: Task): number {
+    return first.deadline.localeCompare(second.deadline);
+  }
+
+  //*ngFor="let item of (tasks | orderBy: 'deadline'); let i = index; let first = first; let last = last"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 import { NgPipesModule } from 'ngx-pipes';
 import localePl from '@angular/common/locales/pl';
-import { registerLocaleData} from '@angular/common';
+import { registerLocaleData } from '@angular/common';
 
 registerLocaleData(localePl);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgPipesModule],
+  imports: [BrowserModule, FormsModule, NgPipesModule,],
   providers: [],
   bootstrap: [AppComponent],
 })
